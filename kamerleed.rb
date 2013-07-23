474faa052efb1bbb2b9ac5d940bb7d2d6a48d098
Breyten Ernsting <bje@dds.nl>
2013-07-23 16:43:04 +0200

    Better looking version. See #2

--- kamerleed.rb
+++ kamerleed.rb
@@ -7,44 +7,52 @@ require 'json'
 require 'sinatra'
 require 'httparty'
 
-def seniority(members)
+def random_member(members)
   kamerlid = members[rand(members.length)]
+  return kamerlid
+end
+
+def seniority(kamerlid)
   title = kamerlid['title']
   seniority = kamerlid['seniority'].to_i
 
   return "%s zit al %s dagen in de tweede kamer!" % [title, seniority]
 end
 
-def age(members)
-  kamerlid = members[rand(members.length)]
+def age(kamerlid)
   title = kamerlid['title']
   age = kamerlid['age'].to_i
+
   return "Wist je dat %s alweer %s jaar oud is?" % [title, age]
 end
 
-def hometown(members)
-  kamerlid = members[rand(members.length)]
+def hometown(kamerlid)
   title = kamerlid['title']
   hometown = kamerlid['hometown']
+
   return "Blijkbaar woont %s in %s!" % [title, hometown]
 end
 
-def birthplace(members)
-  kamerlid = members[rand(members.length)]
+def birthplace(kamerlid)
   title = kamerlid['title']
   hometown = kamerlid['birthplace']
+
   return "%s werd geboren in %s." % [title, hometown]
 end
 
 get '/' do
   @data = {}
   @members = []
-  File.open('data.json', 'r') do |f|
+  File.open('kamerleden.json', 'r') do |f|
     @members = JSON.parse(f.read)
   end
   @options = ['seniority', 'age', 'hometown', 'birthplace']
+
+
+  @kamerlid = random_member(@members)
   @function = @options[Random.rand(@options.length)]
-  @sentence = send(@function, @members)
+  @sentence = send(@function, @kamerlid)
+
   erb :index
 end
 
@@ -52,7 +60,14 @@ get '/update' do
   response = response = HTTParty.get('http://www.tweedekamer.nl/xml/kamerleden.xml')
   #puts response.inspect
   data = response.parsed_response['root']['kamerlid']
-  File.open("data.json","w") do |f|
+  File.open("kamerleden.json","w") do |f|
     f.write(data.to_json)
   end
+
+  response = response = HTTParty.get('http://www.tweedekamer.nl/xml/kamermaps_config.xml')
+  #puts response.inspect
+  data = response.parsed_response['config']
+  File.open("kamermaps.json","w") do |f|
+    f.write(data.to_json)
+  end  
 end
